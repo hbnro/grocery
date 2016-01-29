778ebec3cb4585eccb346e7a4c333755a47f30d4
Alvaro Cabrera <pateketrueke@gmail.com>
2016-01-28 23:11:56 -0600

    Test PDO

--- spec/datasources.php
+++ spec/datasources.php
@@ -3,8 +3,11 @@
 return function(\Closure $lambda) {
   $datasources = array_filter(array(
     'sqlite::memory:',
+    'sqlite::memory:#pdo',
     'mysqli://root@localhost/grocery',
-    getenv('CI') && !defined('HHVM_VERSION') ? 'pgsql://postgres@localhost/grocery' : ''
+    'mysqli://root@localhost/grocery#pdo',
+    (getenv('CI') && !defined('HHVM_VERSION')) ? 'pgsql://postgres@localhost/grocery' : '',
+    (getenv('CI') && !defined('HHVM_VERSION')) ? 'pgsql://postgres@localhost/grocery#pdo' : '',
   ));
 
   array_map(function($conn) use ($lambda) {
